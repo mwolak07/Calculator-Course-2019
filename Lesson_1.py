9ac82b0bb6b65e8081e9f8b62fee08b57a68ebdc
mwolak07 <40680140+mwolak07@users.noreply.github.com>
2019-08-14 13:41:01 -0400

    Added lesson 1

---
+++ Lesson_1.py
@@ -0,0 +1,6 @@
+# Grabbing the first number
+print("Simple Calculator App")
+firstNum = input("Input your first number")
+operator = input("Input operator")
+secondNum = input("Input your second number")
+print("Actual math not implemented")
