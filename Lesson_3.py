b438618007bbdc338cbf990f19eac35254a7e310
mwolak07 <40680140+mwolak07@users.noreply.github.com>
2019-08-19 23:07:35 -0400

    Update Lesson_3.py

--- Lesson_3.py
+++ Lesson_3.py
@@ -3,7 +3,7 @@
 print("Simple Calculator App\n")  # Title
 # Grabbing user input
 firstNum = float(input("Input your first number: "))
-operator = input("Input operator: ")
+operator = str(input("Input operator: "))
 secondNum = float(input("Input your second number: "))
 
 if operator == "+":
